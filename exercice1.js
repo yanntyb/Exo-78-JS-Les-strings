4a25fa858ee66dc6bdcf0adcf139f0194a251928
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-14 09:25:30 +0100

    a

--- exercice1.js
+++ exercice1.js
@@ -2,13 +2,23 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
+
+
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let indexI = monTexte.indexOf("i");
+document.getElementById("position").innerHTML = indexI;
 
+let monTextUpp = monTexte.toUpperCase();
+document.getElementById("chaineMaj").innerHTML = monTextUpp;
 
-
+for(let lettre of monTexte){
+    if(lettre === "a"){
+        alert("a trouvé");
+    }
+}
 
 
 
